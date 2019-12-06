27711a414a9922b700def7cecd3f7826cf1d072c
matthias <mr3lucciola@gmail.com>
2019-12-06 18:06:42 -0500

    deleted client folder

---
+++ build/precache-manifest.e5ce8dff030db2aa9db5347b8c31c96d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "843158a63958e9e9ce7a12b8ca1c663b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4da3bad5664d7748a791",
+    "url": "/static/css/main.507336ad.chunk.css"
+  },
+  {
+    "revision": "c4d77748c6a79a5f9341",
+    "url": "/static/js/2.ab27fb67.chunk.js"
+  },
+  {
+    "revision": "4da3bad5664d7748a791",
+    "url": "/static/js/main.237bbcfb.chunk.js"
+  },
+  {
+    "revision": "1337678ae3301a2c8a7b",
+    "url": "/static/js/runtime-main.8eac158a.js"
+  }
+]);
