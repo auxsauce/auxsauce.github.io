d78fae85b9df3922cbfa6443a3a76435b948fc5c
matthias <mr3lucciola@gmail.com>
2019-12-06 19:22:51 -0500

    updated boilerplate and added components

---
+++ src/components/main/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Projects from '../projects/Projects';
+
+export default function Main(){
+    return(
+        <div className="Main">
+            <Projects />
+        </div>
+    )
+}
